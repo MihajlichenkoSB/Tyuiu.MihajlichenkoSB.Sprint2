9f04db08d50c8cb1d7cdc4a085ce20f328342b48
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-10 19:17:39 +0500

    Task 4

---
+++ Tyuiu.MihajlichenkoSB.Sprint2.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint2.Task4.V2.Lib;
+namespace Tyuiu.MihajlichenkoSB.Sprint2.Task4.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService ds = new DataService();
+
+            double result = ds.Calculate(10, 2);
+            double expected = 10 + (10 + 1) / 2.0;
+            Assert.AreEqual(System.Math.Round(expected, 3), result);
+
+            double res = ds.Calculate(4, 3);
+            double exp = (3 + 5) / (3 + 2.0);
+            Assert.AreEqual(System.Math.Round(expected, 3), result);
+        }
+    }
+}
---
+++ Tyuiu.MihajlichenkoSB.Sprint2.Task4.V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.MihajlichenkoSB.Sprint2.Task4.V2.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 |  Выполнил: Михайличенко С.Б. | ИИПб-25-1";
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* Спринт #2                                                                *");
+        Console.WriteLine("* Тема: Оператор if - полная и короткая форма записи                       *");
+        Console.WriteLine("* Задание #2                                                               *");
+        Console.WriteLine("* Вариант #2                                                               *");
+        Console.WriteLine("* Выполнил: Михайличенко Сергей Борисович | ИИП6-25-1                      *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+        Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры   *");
+        Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в                      *");
+        Console.WriteLine("* заштрихованной области.                                                  *");
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+        Console.WriteLine("****************************************************************************");
+
+        Console.WriteLine("Введите значение x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        double result = ds.Calculate(x, y);
+
+        Console.WriteLine("****************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+        Console.WriteLine("****************************************************************************");
+
+        Console.WriteLine(" z = {result}");
+
+        Console.ReadKey();
+    }
+}
