c28ace8bea4618d7ce8154bd103b827147c434bc
Сергей Михайличенко <sm4269698@gmail.com>
2025-10-12 13:52:04 +0500

    Task 7

--- Tyuiu.MihajlichenkoSB.Sprint2.Task7.V1.Lib/DataService.cs
+++ Tyuiu.MihajlichenkoSB.Sprint2.Task7.V1.Lib/DataService.cs
@@ -1,8 +1,8 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
-namespace Tyuiu.MihajlichenkoSB.Sprint2.Task7.V1.Lib
+namespace Tyuiu.MihajlichenkoSB.Sprint2.Task7.V9.Lib
 {
-    public class DataService : ISprint2Task7V1
+    public class DataService : ISprint2Task7V9
     {
         public bool CheckDotInShadedArea(double x, double y)
         {
